40c3a328c8c7b6aee8a4a21303f873ea819a0c3b
marcelo rivera <marceloriveras@gmail.com>
2023-04-20 16:13:56 -0300

    Projeto final

---
+++ src/controllers/apiController.ts
@@ -0,0 +1,80 @@
+import { Request, Response } from 'express';
+import { Sequelize } from 'sequelize';
+
+import {Phrase } from '../models/Phrase';
+
+export const ping = (req: Request, res: Response) => {
+    res.json({pong: true});
+}
+
+export const random =  (req: Request, res: Response) => {
+    let nRand: number = Math.floor(Math.random() * 100);
+    res.json({number: nRand})
+}
+
+export const nome =  (req: Request, res: Response) => {
+    let nome: string = req.params.nome;
+    res.json({nome: `Você enviou o nome ${nome}`});
+}
+
+export const createPhrase = async (req:Request, res: Response) => {
+    //let author: string = req.body.author;
+    //let txt: string = req.body.txt;    ou
+    let {author, txt} = req.body;
+
+    let newPhrase = await Phrase.create({ author, txt });
+
+    res.json({id: newPhrase.id, author, txt});
+}
+
+export const listPhrases = async (req:Request, res: Response) => {
+    let list = await Phrase.findAll();
+
+    res.json({list});
+}
+
+export const getPhrase = async (req:Request, res: Response) => {
+    let phrase = await Phrase.findByPk(req.params.id);
+    if(phrase){
+        res.json({phrase});
+    }else {
+        res.json({error: 'Frase não encontrada'});
+    }
+}
+
+export const updatePhrase = async (req:Request, res: Response) => {
+    let phrase = await Phrase.findByPk(req.params.id);
+    let { author, txt } = req.body;
+
+    if(phrase){
+        phrase.author = author;
+        phrase.txt = txt;
+        await phrase.save();
+
+        res.json({phrase});
+    }else {
+        res.json({error: 'Frase não encontrada'});
+    }
+}
+
+export const deletePhrase = async (req:Request, res: Response) => {
+    let phrase = await Phrase.destroy({
+        where:{
+            id: req.params.id
+        }
+    });
+    res.json({phrase});
+}
+
+export const randomPhrase = async (req:Request, res: Response) => {
+    let phrase = await Phrase.findOne({
+        order: [
+            Sequelize.fn('RANDOM')  // NO MYSQL => RAND
+        ]
+    });
+    if(phrase){
+    res.json({phrase});
+    }else{
+        res.json({error: 'Não há frases cadastradas !'});
+    }
+}
--- src/routes/api.ts
+++ src/routes/api.ts
@@ -1,19 +1,18 @@
 import { Router } from 'express';
 
+import * as ApiController from '../controllers/apiController';
+
 const router = Router();
 
-router.get('/ping',(req, res) => {
-    res.json({pong: true});
-});
+router.get('/ping', ApiController.ping);
+router.get('/random',ApiController.random);
+router.get('/nome/:nome', ApiController.nome);
+router.get('/frases/aleatoria',ApiController.randomPhrase);
 
-router.get('/random',(req,res) => {
-    let nRand: number = Math.floor(Math.random() * 100);
-    res.json({number: nRand})
-});
-
-router.get('/nome/:nome', (req, res) => {
-    let nome: string = req.params.nome;
-    res.json({nome: `Você enviou o nome ${nome}`});
-});
+router.post('/frases', ApiController.createPhrase);
+router.get('/frases', ApiController.listPhrases);
+router.get('/frases/:id', ApiController.getPhrase);
+router.put('/frases/:id',ApiController.updatePhrase);
+router.delete('/frases/:id',ApiController.deletePhrase);
 
 export default router;
